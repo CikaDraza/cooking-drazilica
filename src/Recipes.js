f67260e84b0653ffe5b68d6c293456db1e63e58e
Drazilica <drazic.milan@gmail.com>
2019-10-29 00:16:09 +0100

    initial commit

--- src/Recipes.js
+++ src/Recipes.js
@@ -13,10 +13,10 @@ const Recipe = ({title, calories, totalWeight, image, source})=> {
           <a href="https://www.youtube.com/watch?v=4bejSQdhVeo" className="save-recipes"><img src="set-icons-bookmark-add.png" /><span>save</span></a>
             <img className="pic" src={image} alt=""/>
             <h4 className="title">{title}</h4>
+          </a>
             <hr />
             <h5><span className='App-link cal'>{cal}</span> kcal<span className='App-link weight'> {totalW}</span> g</h5>
             <hr />
-          </a>
           <h5 className="source"><a href="#">{source}</a></h5>
         </div>
       </div>
