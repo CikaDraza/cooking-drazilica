5396f692421a770af37e735f990482cfde922ddf
Drazilica <drazic.milan@gmail.com>
2019-10-29 01:14:42 +0100

    Updates

---
+++ precache-manifest.dfcd8db0ac59212f972ac105e3849866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f348947809966b0c65d1caacc3324da7",
+    "url": "/drazilica-recipes/index.html"
+  },
+  {
+    "revision": "04f227b5e1b51f806b21",
+    "url": "/drazilica-recipes/static/css/main.d5ef84e2.chunk.css"
+  },
+  {
+    "revision": "828291802bf045a9fe2b",
+    "url": "/drazilica-recipes/static/js/2.56fcdf75.chunk.js"
+  },
+  {
+    "revision": "04f227b5e1b51f806b21",
+    "url": "/drazilica-recipes/static/js/main.3aa2db84.chunk.js"
+  },
+  {
+    "revision": "dd20ee0c24ffd0c4e6bb",
+    "url": "/drazilica-recipes/static/js/runtime-main.e117b7d8.js"
+  }
+]);
